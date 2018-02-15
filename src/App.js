97709fc2295491ed2ecded365f19a4d2950c75f7
MariaSyed <mariasy@metropolia.fi>
2018-02-16 00:11:12 +0200

    added remove button

--- src/App.js
+++ src/App.js
@@ -1,76 +1,91 @@
 import React, { Component } from "react";
-import firebase from 'firebase'
+import firebase from "firebase";
 import "./App.css";
 import Dropzone from "react-dropzone";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 
 var config = {
-    apiKey: "AIzaSyBCvx1WFPYOutNvj8Hfmu_jTuxoWEQvPHc",
-    authDomain: "isdp-954b3.firebaseapp.com",
-    databaseURL: "https://isdp-954b3.firebaseio.com",
-    projectId: "isdp-954b3",
-    storageBucket: "isdp-954b3.appspot.com",
-    messagingSenderId: "195263147810"
+  apiKey: "AIzaSyBCvx1WFPYOutNvj8Hfmu_jTuxoWEQvPHc",
+  authDomain: "isdp-954b3.firebaseapp.com",
+  databaseURL: "https://isdp-954b3.firebaseio.com",
+  projectId: "isdp-954b3",
+  storageBucket: "isdp-954b3.appspot.com",
+  messagingSenderId: "195263147810"
 };
 
 if (firebase.apps.length === 0) {
-    firebase.initializeApp(config);
+  firebase.initializeApp(config);
 }
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        readings: [],
-        dropzoneText: 'Dropping your csv file here, or click to select file to upload.',
-        fileIncorrect: false
+      readings: [],
+      dropzoneText:
+        "Dropping your csv file here, or click to select file to upload.",
+      fileIncorrect: false
     };
+
+    this.readingsRef = firebase.database().ref('readings')
   }
 
-  componentDidMount() {
-      firebase.database().ref('/readings').on('value', (snapshot) => {
-          if (snapshot.val()) {
-              console.log('values! ', snapshot.val())
-              const readings = Object.values(snapshot.val())
-              this.setState({ readings })
-          }
-      })
+  componentWillMount() {
+   this.readingsRef
+      .on("value", snapshot => {
+        if (snapshot.val()) {
+          const readings = Object.values(snapshot.val());
+          this.setState({ readings });
+        }
+      });
   }
 
   onDrop(e) {
     if (e[0]) {
       const reader = new FileReader();
       reader.onload = () => {
-        const csv = reader.result.replace(/\r/g,'')
+        const csv = reader.result.replace(/\r/g, "");
         let lines = csv.split("\n");
 
         let headers = lines[0].split(",");
 
-        if (headers.find((h) => h.includes('date') || h.includes('temperature'))) {
+        if (
+          headers.find(h => h.includes("date") || h.includes("temperature"))
+        ) {
+          for (let i = 1; i < lines.length; i++) {
+            let obj = {};
+            let currentline = lines[i].split(",");
 
-            for (let i = 1; i < lines.length; i++) {
-                let obj = {};
-                let currentline = lines[i].split(",");
-
-
-                for (let j = 0; j < headers.length; j++) {
-                    if (currentline[j] && headers[j]) {
-                        obj[headers[j].replace(/\./g, '')] = currentline[j];
-                    }
-                }
-
-                firebase.database().ref('/readings').push(obj)
+            for (let j = 0; j < headers.length; j++) {
+              if (currentline[j] && headers[j]) {
+                obj[headers[j].replace(/\./g, "")] = currentline[j];
+              }
             }
+
+              this.readingsRef.push(obj);
+          }
         } else {
-            this.setState({ dropzoneText: 'Incorrect fields in file! Could not parse', fileIncorrect: true })
+          this.setState({
+            dropzoneText: "Incorrect fields in file! Could not parse",
+            fileIncorrect: true
+          });
         }
-      }
+      };
 
       reader.readAsBinaryString(e[0]);
     } else {
-        this.setState({ dropzoneText: 'Incorrect file format!', fileIncorrect: true   })
+      this.setState({
+        dropzoneText: "Incorrect file format!",
+        fileIncorrect: true
+      });
     }
+  }
+
+  deleteAllData() {
+      if (window.confirm('Do you really want to delete all data?')) {
+          this.readingsRef.set(null)
+      }
   }
 
   render() {
@@ -78,13 +93,19 @@ class App extends Component {
       <div>
         <div className="dropzone">
           <Dropzone onDrop={this.onDrop.bind(this)}>
-            <p style={this.state.fileIncorrect ? { color: 'red' } : {}}>
-                { this.state.dropzoneText }
+            <p style={this.state.fileIncorrect ? { color: "red" } : {}}>
+              {this.state.dropzoneText}
             </p>
           </Dropzone>
         </div>
 
-          <div className="instructions"><i>Click on headings to sort</i></div>
+        <div className="instructions">
+          <i>Click on headings to sort</i>
+        </div>
+
+          <button className="reset-btn" onClick={this.deleteAllData}>
+              Click here to remove all data
+          </button>
 
         <div>
           <ReactTable
